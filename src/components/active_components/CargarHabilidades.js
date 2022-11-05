a022de4a5b0641792f6e6544697b703f7d455b85
javierhuebra <javieremanuelhuebra@gmail.com>
2022-11-05 04:29:42 -0300

    feat: se agrega la vista de agregar habilidades

--- src/components/active_components/CargarHabilidades.js
+++ src/components/active_components/CargarHabilidades.js
@@ -1,7 +1,44 @@
+import '../../assets/stylesheets/active_components/CargarHabilidades.css'
+import { IoIosTrash } from "react-icons/io";
 const CargarHabilidades = () =>{
     return(
-        <div>
-            cargar habilidades - wip
+        <div className="habilidades-cont">
+
+            <form className="form-habilidades">
+                <input className='input-habilidad' type="text" placeholder="Habilidad..." autoComplete="false"/>
+                <input className='input-cargar' type="submit" value="Cargar"/>
+            </form>
+
+            <div className="habilidades-en-db">
+                <div className="tit-hab">
+                    Habilidades Cargadas
+                </div>
+
+                {/* ACÁ IRIA EL MAP QUE LEE DE LA DB LAS HABILIDADES CARGADAS */}
+                <div className='habilidad'>
+                    Manejo Backend
+                    <div className='btn-delete'><IoIosTrash/></div>
+                </div>
+
+                <div className='habilidad'>
+                    Manejo Backend
+                    <div className='btn-delete'><IoIosTrash/></div>
+                </div>
+
+                <div className='habilidad'>
+                    Manejo Backend
+                    <div className='btn-delete'><IoIosTrash/></div>
+                </div>
+
+                <div className='habilidad'>
+                    Manejo Backend
+                    <div className='btn-delete'><IoIosTrash/></div>
+                </div>
+                
+               
+            </div>
+
+
         </div>
     )
 }
