bbeb1379ea69da6c936ed7eb0562577dfa8808bd
javierhuebra <javieremanuelhuebra@gmail.com>
2022-11-05 15:11:33 -0300

    feat: se agrega la pantalla cargar estudiante

--- src/components/active_components/CargarEstudiante.js
+++ src/components/active_components/CargarEstudiante.js
@@ -1,7 +1,24 @@
+import '../../assets/stylesheets/active_components/CargarEstudiante.css'
+
 const CargarEstudiante = () =>{
     return(
-        <div>
-            cargar estudiante - wip
+        <div className="estudiante-cont">
+            <form className="form-estudiante">
+                
+                    <label htmlFor="nombre">Nombre</label>
+                    <input className='est-text' type='text' name="nombre" placeholder='Nombre y Apellido'/>
+                
+                
+                    <label htmlFor="email">Email</label>
+                    <input className='est-text' type='text' name="email" placeholder='estudiante@skill.com'/>
+                
+                <input className='btn-cargar' type='submit' value="Cargar"/>
+            </form>
+            <p className='texto-recordatorio'>
+                Tambien se podría agregar que cuando se
+                carga el estudiante se inicialice cons
+                las habilidades que esten seteadas en bd
+            </p>
         </div>
     )
 }
