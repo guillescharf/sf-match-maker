8d75f8c841edb4747c3ede80de2e9165b8684590
Guillermo Scharf <100360133+guillescharf@users.noreply.github.com>
2022-11-05 21:56:18 -0300

    :memo: Commented code included to have to implement.

--- src/components/active_components/VerGrupos.js
+++ src/components/active_components/VerGrupos.js
@@ -1,7 +1,22 @@
 const VerGrupos = () =>{
+
+/*     const exportData = () => {
+        const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+          JSON.stringify(data)
+        )}`;
+        const link = document.createElement("a");
+        link.href = jsonString;
+        link.download = "data.json";
+    
+        link.click();
+    };    
+ */
     return(
         <div>
             ver grupos - wip
+{/*             <button type="button" onClick={exportData}>
+                Export Data
+            </button> */}
         </div>
     )
 }
