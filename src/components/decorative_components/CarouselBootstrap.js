4c9fa803c40194b5569cbdb6380adb834ab2c052
javierhuebra <javieremanuelhuebra@gmail.com>
2022-11-15 00:12:37 -0300

    styling, agregando imagenes

--- src/components/decorative_components/CarouselBootstrap.js
+++ src/components/decorative_components/CarouselBootstrap.js
@@ -1,6 +1,8 @@
 import Carousel from 'react-bootstrap/Carousel';
 import '../../assets/stylesheets/decorative_components/CarouselBootstrap.css'
 import image1 from '../../assets/image/group1.png';
+import image2 from '../../assets/image/imagen02.png';
+import image3 from '../../assets/image/imagen03.png';
 function CarouselBootstrap() {
   return (
     <Carousel>
@@ -19,23 +21,22 @@ function CarouselBootstrap() {
       <Carousel.Item interval={1000}>
         <img
           className="d-block w-100 img-carousel"
-          src="https://puentedelmundo.com/wp-content/uploads/2021/01/pexels-sharon-mccutcheon-1209843.jpg"
+          src={image2}
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h3>Imagen descriptiva 2</h3>
-          <p>Muestra alguna utilidad de la app, valor, etc</p>
+         
+          
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={1000}>
         <img
           className="d-block w-100 img-carousel"
-          src="https://puentedelmundo.com/wp-content/uploads/2021/01/pexels-sharon-mccutcheon-1209843.jpg"
+          src={image3}
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h3>Imagen descriptiva 3</h3>
-          <p>Muestra alguna utilidad de la app, valor, etc</p>
+         
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
